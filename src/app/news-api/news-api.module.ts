41cd1b6cbac0c1aa13bafd8c8da687e711fdaba3
Ruben Lukes <lukesruben@gmail.com>
2020-06-12 23:03:59 +0200

    adding paginator components

--- src/app/news-api/news-api.module.ts
+++ src/app/news-api/news-api.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NaArticleListComponent } from './na-article-list/na-article-list.component';
 import { TrimOUtletNamePipe } from './trim-outlet-name.pipe';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [NaArticleListComponent, TrimOUtletNamePipe],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   exports: [NaArticleListComponent],
 })
 export class NewsApiModule {}
--- src/app/shared/paginator/paginator.component.ts
+++ src/app/shared/paginator/paginator.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-paginator',
   templateUrl: './paginator.component.html',
-  styleUrls: ['./paginator.component.css']
+  styleUrls: ['./paginator.component.css'],
 })
 export class PaginatorComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  @Input() numberOfPages = 5;
+  currentPage = 1;
+  pageOptions: number[];
+  constructor() {
+    this.pageOptions = [
+      this.currentPage - 2,
+      this.currentPage - 1,
+      this.currentPage,
+      this.currentPage + 1,
+      this.currentPage + 2,
+    ].filter((p) => p >= 1 && p <= this.numberOfPages);
   }
 
+  ngOnInit(): void {}
 }
